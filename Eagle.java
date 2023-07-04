c88a8e39eb9986c4e2beb92e5d0014dec0079bfd
julien himmer <julienhimmer@MacBook-Pro-de-julien.local>
2023-07-04 16:56:17 +0200

    homework done

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,70 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff()
+    {
+        if(this.altitude == 0)    
+        {
+            this.flying =true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+
+    }
+
+    @Override
+    public int ascend(int meters)
+    {
+        if(this.flying)
+        {
+            this.altitude = Math.max(this.altitude+meters , meters);
+            System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        }
+
+        return this.altitude;
+
+    }
+
+    @Override
+    public void glide()
+    {
+        if(this.flying && this.altitude > 0)
+        {
+            System.out.printf("glides into the air.%n");
+        }
+    }
+
+    @Override
+    public int descend (int meters)
+    {
+        if(this.flying)
+        {
+            this.altitude = Math.min(this.altitude - meters, 325);
+            System.out.printf("%s flies downward, altitude: %d%n", this.getName(), this.altitude);
+        }
+
+        return this.altitude;
+    }  
+
+    @Override
+    public void land ()
+    {
+        if(this.altitude < 10)
+        {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        }
+        else 
+        {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
+}//
+
+
+
+
+
+
+
+
 }
--- Nature.java
+++ Nature.java
@@ -11,17 +11,36 @@ public class Nature {
         pingou.swimUp(1);
         pingou.getOut();
 
+
+        System.out.println(" ");
+
+
         Eagle hawkeye = new Eagle("Hawkeye");
         // TODO : uncomment the following code in order to test it
-        /*
+
+        //Hawkeye takes off in the sky.
         hawkeye.takeOff();
+
+        //Hawkeye flies upward, altitude: 120
         hawkeye.ascend(120);
+
+        //Hawkeye flies upward, altitude: 150
         hawkeye.ascend(30);
+
+        //glides into the air.
         hawkeye.glide();
+
+        //Hawkeye flies downward, altitude: 10
         hawkeye.descend(140);
+
+        //Hawkeye is too high, it can't land.
         hawkeye.land();
+
+        //Hawkeye flies downward, altitude: 1
         hawkeye.descend(9);
+
+        //Hawkeye lands on the ground.
         hawkeye.land();
-        */
+
     }
 }
